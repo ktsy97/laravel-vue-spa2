fdec0e0136c07eef72f142005b8d93f64c671119
KONDO Katsuya <k0d0ktsy@gmail.com>
2022-04-23 10:02:18 +0900

    ログアウトをモーダル化

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -27,12 +27,34 @@
             @else
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item d-lg-flex align-items-center">
-                    <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <a class="nav-link" href="" data-bs-toggle="modal" data-bs-target="#logoutModal">
                         {{ __('logout') }}
                     </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered modal-sm">
+                            <div class="modal-content p-3">
+                                <div class="title d-flex">
+                                    <button type="button" class="btn-close ms-auto" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body p-0">
+                                    <p class="text-center pb-3">ログアウトしますか？</p>
+
+                                    <a class="btn btn-primary rounded-pill w-100 mb-1" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        OK
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+
+                                    <button type="button" class="btn btn-secondary rounded-pill w-100" data-bs-dismiss="modal">
+                                        キャンセル
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </li>
             </ul>
             @endguest
